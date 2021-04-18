b4222dee6e7a035fe9e8d568f5eb2371a9da4105
David Shapiro <daveshap37@gmail.com>
2021-04-18 11:00:13 -0400

    initial success

--- generate_actions_curie.py
+++ generate_actions_curie.py
@@ -1,6 +1,12 @@
+import json
 import re
 import openai
 import datetime as dt
+
+
+def write_json(filename, data):
+    with open(filename, 'w') as outfile:
+        json.dump(data, outfile, indent=1)
 
 
 def read_file(filename):
@@ -32,12 +38,15 @@ open_ai_api_key = read_file('openaiapikey.txt')
 openai.api_key = open_ai_api_key
 txt = str(dt.datetime.now())
 contextfilename = 'raw_contexts/ctx_2021-04-17T10-10-12.txt'
-actionfilename = 'raw_actions/act_%s.txt' % txt.replace(':','-').replace(' ','T').split('.')[0]
+actionfilename = 'raw_actions/act_%s.json' % txt.replace(':','-').replace(' ','T').split('.')[0]
+
 
 base_prompt = '''Generate a list of possible actions in response to the context. 
 
 CONTEXT: %s
 ACTION:'''
+
+# TODO experiment with prompts (some specific for dialog, some for Raven POV, etc)
 
 
 def clean_line(txt):
@@ -50,25 +59,32 @@ def clean_line(txt):
     digit = re.match('''^\d+.?\s*''', txt)
     if digit:
         txt = txt.replace(digit.group(), '')
+    # clean off lettered list
+    letter = re.match('''^\w\)\s*''', txt)
+    if letter:
+        txt = txt.replace(letter.group(), '')
     return txt
     
 
 def generate_actions(prompt):
+    result = list()
     content = query_gpt3(prompt)
     lines = content.splitlines() 
     for line in lines:
         line = clean_line(line)
         if len(line) == 0:
             continue
-        print(line)
-        append_file(actionfilename, line)
+        print('ACTION: ', line)
+        result.append(line)
+    return result
 
 
 if __name__ == '__main__':
+    data = list()
     contexts = read_file(contextfilename)
     for i in contexts.splitlines():
-        print('CONTEXT: ', i)
+        print('\n\nCONTEXT: ', i)
         prompt = base_prompt % i
-        append_file(actionfilename, '\n\nCONTEXT: %s' % i)
-        generate_actions(prompt)
-        #exit(0)
+        actions = generate_actions(prompt)
+        data.append({'context': i, 'actions': actions})
+    write_json(actionfilename, data)
